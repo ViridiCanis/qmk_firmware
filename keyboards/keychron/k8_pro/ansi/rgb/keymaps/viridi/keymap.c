0306ff0896d1563c29c3b689e3e48fca3e725b4c
Viridi <virididev@gmail.com>
2023-05-31 14:48:47 +0200

    refactor+feat: numpad near wasd, move macros around, split into extra layer and extra with macros using dipswitch

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/viridi/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/viridi/keymap.c
@@ -21,48 +21,65 @@
 // clang-format off
 enum layers {
     BASE,
-    FN,
-    EXTRA
+    EXTRA_SIMPLE,
+    EXTRA_MACROS,
+    FN
 };
+
+#define EXT_SMP EXTRA_SIMPLE
+#define EXT_MCR EXTRA_MACROS
 
 enum my_keycodes {
     MY_DELAY = SAFE_RANGE,
+    EXTRA_SWITCH,
     TURBO_CLICK,
     TURBO_REPEAT,
     TURBO_REPEAT_SET,
     R_CHAT
 };
 
-#define MT_CAPS MT(MOD_RSFT, KC_CAPS)
-
+#define EXT_SWT EXTRA_SWITCH
 #define TRB_CLK TURBO_CLICK
 #define TRB_REP TURBO_REPEAT
 #define TRB_SET TURBO_REPEAT_SET
+
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+#define MT_CAPS MT(MOD_RSFT, KC_CAPS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ansi_87(
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,   KC_CTANA, RGB_MOD,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,    KC_END,   KC_PGDN,
-    OSL(EXTRA),KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+     EXT_SWT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                      MT_CAPS,             KC_UP,
      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI,  OSL(FN),  KC_RCTL,  KC_LEFT,   KC_DOWN,  KC_RGHT),
 
-[FN] = LAYOUT_ansi_87(
-     _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,            _______,  _______,  RGB_TOG,
-     _______,  BT_HST1,  BT_HST2,  BT_HST3,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-     _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-     _______,  _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  DM_REC1,  DM_PLY1,  MY_DELAY,                     _______,            _______,
-     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______),
+[EXTRA_SIMPLE] = LAYOUT_ansi_87(
+     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,
+     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PSLS,  KC_PAST,  KC_PMNS,  KC_PPLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+     XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  KC_P7,    KC_P8,    KC_P9,    KC_PDOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+     _______,  _______,  _______,  _______,  KC_P4,    KC_P5,    KC_P6,    KC_P0,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,
+     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_P1,    KC_P2,    KC_P3,    KC_PENT,  KC_NUM,   XXXXXXX,  XXXXXXX,                      XXXXXXX,            XXXXXXX,
+     _______,  _______,  _______,                                XXXXXXX,                                _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX),
 
-[EXTRA] = LAYOUT_ansi_87(
-     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
-     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-     _______,  QK_LOCK,  _______,  _______,  R_CHAT,   TRB_CLK,  _______,  _______,  KC_KP_7,  KC_KP_8,  KC_KP_9,  _______,  _______,  _______,  _______,  _______,  _______,
-     _______,  TRB_SET,  TRB_REP,  _______,  _______,  _______,  _______,  _______,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_NUM,             _______,
-     _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_KP_0,  KC_KP_1,  KC_KP_2,  KC_KP_3,                      _______,            _______,
-     _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______),
+[EXTRA_MACROS] = LAYOUT_ansi_87(
+     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,
+     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+     XXXXXXX,  QK_LOCK,  _______,  XXXXXXX,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            R_CHAT,
+     _______,  TRB_SET,  TRB_REP,  TRB_CLK,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,                      XXXXXXX,            XXXXXXX,
+     _______,  _______,  _______,                                XXXXXXX,                                _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX),
+
+[FN] = LAYOUT_ansi_87(
+     XXXXXXX,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,            XXXXXXX,  XXXXXXX,  RGB_TOG,
+     XXXXXXX,  BT_HST1,  BT_HST2,  BT_HST3,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+     _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,
+     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  BAT_LVL,  NK_TOGG,  XXXXXXX,  DM_REC1,  DM_PLY1,  MY_DELAY,                     XXXXXXX,            XXXXXXX,
+     _______,  _______,  _______,                                XXXXXXX,                                _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX),
 
 // [x] = LAYOUT_ansi_87(
 //      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
@@ -77,15 +94,38 @@ struct TurboRepeatState turbo_click_state;
 struct TurboRepeatState turbo_repeat_state;
 
 void keyboard_post_init_user(void) {
-    debug_enable = true;
-
     turbo_click_state = create_state(KC_MS_BTN1);
     turbo_repeat_state = create_state(KC_A);
+}
+
+static uint8_t extra_layer;
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+    if (index == 0) {
+        layer_off(extra_layer);
+        if (active)  {
+            extra_layer = EXTRA_MACROS;
+        } else {
+            extra_layer = EXTRA_SIMPLE;
+        }
+    }
+
+    return false; // override default functionality
 }
 
 static bool awaiting_turbo_repeat_set = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (keycode == EXTRA_SWITCH) {
+        if (record->event.pressed) {
+            layer_on(extra_layer);
+        } else {
+            layer_off(extra_layer);
+        }
+
+        return false;
+    }
+
     if (record->event.pressed && awaiting_turbo_repeat_set) {
         awaiting_turbo_repeat_set = false;
         turbo_repeat_state.keycode_to_repeat = keycode;
@@ -136,14 +176,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-bool dip_switch_update_user(uint8_t index, bool active) {
-    if (index == 0) {
-        default_layer_set(BASE);
-    }
-
-    return true;
-}
-
 bool dyn_macro_is_recording = false;
 
 void dynamic_macro_record_start_user(void) {
@@ -165,7 +197,7 @@ bool rgb_matrix_indicators_user(void) {
         rgb_matrix_set_color(0x34, 255, 0, 0);
     }
     if (host_keyboard_led_state().num_lock) {
-        rgb_matrix_set_color(0x3d, 255, 0, 0);
+        rgb_matrix_set_color(0x47, 255, 0, 0);
     }
 
     return true;
